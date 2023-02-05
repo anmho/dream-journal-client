19c812288e9f9d7c583091e6443e62dcef17246e
Benji Lin <benjilin@dhcp-v092-091.mobile.uci.edu>
2023-02-04 17:10:38 -0800

    changed stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import './App.css';
+import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid'
   
 // function App() {
 
@@ -18,22 +20,28 @@ import './App.css';
 //   )
 // }
 function App() {
-  function button(){
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
+  const[todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+
+  function handleAddTodo(e){
+    const name = todoNameRef.current.value
+    if(name === '') return
+    setTodos(prevTodos =>{
+      return[...prevTodos,{id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
   }
+
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg">
-        React and Tailwind CSS in action
+      <p className="text-3xl text-gray-700 font-bold mb-5 text-right">
+        Dream Journal
       </p>
 
-      {/* <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button> */}
-      {button}
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
-      
+      <input ref = {todoNameRef} type = "text"/>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick = {handleAddTodo}>Button</button>
+      <TodoList todos ={todos}/>
     </div>
 
     
