2ca9992743ba27005016d0c6f2fb4af62516a11d
Andrew Masuda <masudaas@uci.edu>
2023-02-04 19:34:38 -0800

    login page

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,12 +3,11 @@ import React from 'react'
 export default function Sidebar() {
   return (
     <>
-    <div className="fixed justify-start h-screen w-1/6 bg-[#01002C] p-8">
+    <div className="fixed top-0 justify-start h-screen w-1/6 bg-gradient-to-r from-[#01002C] to-blue-500 p-8">
         <p className="text-3xl text-white font-bold mb-5 color-white">
             Settings        
-            <img src="pngegg.png" height="30" width="30" margin-left/>
+            <img src="pngegg.png" alt="settingsicon" height="30" width="30" margin-left/>
         </p>
-
     </div>
     </>
   )
